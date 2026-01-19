e9af1cf958be37512571d8a11b09487fb082fd1e
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2026-01-19 12:12:17 +0530

    refactored email-notification

--- creational/simple_factory/ex1_email_notification/main.go
+++ creational/simple_factory/ex1_email_notification/main.go
@@ -2,29 +2,51 @@ package main
 
 import "fmt"
 
-// -- Concrete Implementation ---
-type EmailNotification struct{
+// --- behaviour/contract ---
+type Notifier interface {
+	Send(message string, userID int)
+}
+
+// -- Concrete Implementation EMAIL---
+type EmailNotification struct {
 	sender string
 }
 
-func (e *EmailNotification) Send(message string ,userID int) {
-	fmt.Printf("Sending Email to User %d from %s: %s \n",userID, e.sender, message)
+func (e *EmailNotification) Send(message string, userID int) {
+	fmt.Printf("Sending Email to User %d from %s: %s \n", userID, e.sender, message)
+}
+
+// -- Concrete Implementation SMS ---
+type SMSNotification struct {
+	sender string
+}
+
+func (s *SMSNotification) Send(message string, userID int) {
+	fmt.Printf("Sending SMS to User %d from %s: %s \n", userID, s.sender, message)
 }
 
 // --- business logic ---
 type NotificationService struct {
-	notifier *EmailNotification
+	notifier Notifier
 }
 
 func (n *NotificationService) NotifyUser(userID int, msg string) {
 	n.notifier.Send(msg, userID)
 }
 
-func main() {
-	emailNotif := &EmailNotification{sender:"admin@app.com"}
-
-	service := &NotificationService{
-		notifier : emailNotif,
+// Constructor function / builder function
+func NewNotificationService(n Notifier) *NotificationService {
+	return &NotificationService{
+		notifier: n,
 	}
-	service.NotifyUser(42,"Welcome to our platform!")
+}
+
+func main() {
+	emailNotif := &EmailNotification{sender: "admin@app.com"}
+	smsNotif := &SMSNotification{sender: "admin2@app.com"}
+	service1 := NewNotificationService(emailNotif)
+	service2 := NewNotificationService(smsNotif)
+
+	service1.NotifyUser(42, "Welcome to our platform!")
+	service2.NotifyUser(24, "hello from the other side")
 }
