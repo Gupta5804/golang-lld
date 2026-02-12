8f03f3494818c05db36cdb8b56501a1089773b29
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2026-02-12 17:14:15 +0530

    functional ex2 complete

--- creational/functional_options/ex2_query_builder/main.go
+++ creational/functional_options/ex2_query_builder/main.go
@@ -1,1 +1,50 @@
 package main
+
+type SelectQuery struct {
+	tableName string
+	columns   []string
+	where     string
+	orderBy   string
+	limit     int
+}
+
+type Option func(*SelectQuery)
+
+func WithLimit(n int) Option {
+	return func(s *SelectQuery) {
+		s.limit = n
+	}
+}
+
+func WithWhere(where string) Option {
+	return func(s *SelectQuery) {
+		s.where = where
+	}
+}
+
+func WithOrderBy(orderBy string) Option {
+	return func(s *SelectQuery) {
+		s.orderBy = orderBy
+	}
+}
+
+func WithColumns(cols ...string) Option {
+	return func(s *SelectQuery) {
+		s.columns = cols
+	}
+}
+
+func NewSelectQuery(table string, opts ...Option) *SelectQuery {
+	query := &SelectQuery{
+		tableName: table,
+		columns:   []string{"*"},
+		where:     "",
+		orderBy:   "",
+		limit:     0,
+	}
+
+	for _, opt := range opts {
+		opt(query)
+	}
+	return query
+}
